1e0f86e72369502914d88b2ec9424839430146c6
Minh Tri Ha <minhtrihaa@gmail.com>
2025-09-18 15:55:16 +0200

    Add renvoi dun formulair tsx

--- app/menu/prime-de-naissance-anticipee.tsx
+++ app/menu/prime-de-naissance-anticipee.tsx
@@ -1,10 +1,25 @@
+import { ActionButton } from "@/components/custom/action-button";
+import { FormFieldWithIcon } from "@/components/custom/form-field-with-icon";
 import { Text, View } from "react-native";
 
 export default function PrimeDeNaissanceAnticipeeScreen() {
   return (
     <View className="flex-1 bg-background">
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-lg text-primary">Prime de naissance anticipée</Text>
+      <View className="flex-1 px-6 pt-16 flex flex-col gap-5 justify-start">
+        <Text className="text-lg text-foreground mb-48">
+          Pour une prime de naissance anticipée, il suffit de nous envoyer
+          l’attestation de votre médecin ou sage-femme. Veuillez prendre une
+          photo de ce document
+        </Text>
+        <FormFieldWithIcon
+          title="Document"
+          icon="cloud-upload-outline"
+          placeholder="Choisissez un fichier"
+          className="mb-0"
+        />
+        <ActionButton icon="camera-outline" onPress={() => {}}>
+          Prendre une photo
+        </ActionButton>
       </View>
     </View>
   );
--- app/menu/renvoi-formulaire.tsx
+++ app/menu/renvoi-formulaire.tsx
@@ -1,10 +1,49 @@
+import { ActionButton } from "@/components/custom/action-button";
+import { FormFieldWithIcon } from "@/components/custom/form-field-with-icon";
 import { Text, View } from "react-native";
 
 export default function RenvoiFormulaireScreen() {
   return (
     <View className="flex-1 bg-background">
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-lg text-primary">Renvoi d’un formulaire</Text>
+      <View className="flex-1 px-4 pt-8 flex flex-col gap-5 justify-start">
+        <View className="bg-accent rounded-2xl p-6 ">
+          <Text className="text-background text-lg mb-2 font-semibold">
+            Voulez-vous renvoyer un formulaire?
+          </Text>
+          <Text className="text-background text-base">
+            1. Scannez le formulaire, dûment complété et signé, et
+            enregistrez-le comme fichier PDF, JPEG, GIF ou PNG.{"\n"}
+            2. Complétez le code-barres{"\n"}
+            3. Chargez le fichier via “Choisissez un fichier”.{"\n"}
+            4. envoyez le document
+          </Text>
+        </View>
+        <FormFieldWithIcon
+          title="Code-barres"
+          icon="barcode"
+          placeholder="IIII"
+        />
+        <FormFieldWithIcon
+          title="Document"
+          icon="cloud-upload-outline"
+          placeholder="Choisissez un fichier"
+        />
+        <ActionButton icon="camera-outline" onPress={() => {}}>
+          Prendre une photo
+        </ActionButton>
+      </View>
+      <View className="px-4 pb-8 flex flex-col gap-4">
+        <ActionButton icon="send" onPress={() => {}}>
+          Envoyer
+        </ActionButton>
+        <ActionButton
+          icon="delete-outline"
+          onPress={() => {}}
+          className="bg-gray-300 text-white"
+          disabled
+        >
+          Supprimer
+        </ActionButton>
       </View>
     </View>
   );
